26692ed5aad6b593037720cc22192bc6b5bbb84c
shakil <shakil94196@gmail.com>
2024-01-17 01:53:21 +0600

    Spring Boot Project_b

--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.Student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +11,19 @@ import java.util.List;
 @Service
 public class StudentService {
 
+    private final StudentRepository studentRepository;
+
+    @Autowired
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student>getStudents(){
-        return List.of(
+        return studentRepository.findAll();
+
+
+                /*List.of(
 
                 new Student(
                         1L,
@@ -20,6 +32,6 @@ public class StudentService {
                         LocalDate.of(2000, Month.MARCH,3),
                         21
                 )
-        );
+        );*/
     }
 }
